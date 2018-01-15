d217c10343169dfa0d70d6c8a8357de547ee1701
antonio63j <antonio63j@gmail.com>
2018-01-15 09:06:30 +0100

    incorporamos las modificaciones para deploy en github.com

--- src/app/directivamenu.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DirectivamenuDirective } from './directivamenu.directive';
-
-describe('DirectivamenuDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DirectivamenuDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/mispipes/pipeeuro2.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Pipeeuro2Pipe } from './pipeeuro2.pipe';
-
-describe('Pipeeuro2Pipe', () => {
-  it('create an instance', () => {
-    const pipe = new Pipeeuro2Pipe();
-    expect(pipe).toBeTruthy();
-  });
-});
